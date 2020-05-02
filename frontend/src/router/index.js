ba823679bda2dc80c04570a5ce0c6210c2c854b8
Olzindel <timo.thibault@gmail.com>
2020-05-02 02:39:56 +0200

    feat create product page component and routing to it

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ProductPage from '../components/product-page/ProductPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/product',
+      name: 'Product-Page',
+      component: ProductPage
     }
   ]
 })
