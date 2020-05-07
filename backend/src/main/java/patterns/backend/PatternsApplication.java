5c003a45a242d61a76a231d7bc8fdc720dcfb9f6
alexellero <alexandre.ellero@gmail.com>
2020-05-07 09:23:21 +0200

    chore : add Spotless plugin

--- backend/src/main/java/patterns/backend/PatternsApplication.java
+++ backend/src/main/java/patterns/backend/PatternsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatternsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PatternsApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(PatternsApplication.class, args);
+  }
 }
--- backend/src/test/java/patterns/backend/PatternsApplicationTests.java
+++ backend/src/test/java/patterns/backend/PatternsApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PatternsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
