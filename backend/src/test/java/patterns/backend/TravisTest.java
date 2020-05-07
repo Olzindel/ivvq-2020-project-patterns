bff6383fba659504e697038eca88fac7fd38eea6
alexellero <alexandre.ellero@gmail.com>
2020-05-07 11:44:29 +0200

    fix : test jacoco

---
+++ backend/src/test/java/patterns/backend/TravisTest.java
@@ -0,0 +1,12 @@
+package patterns.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TravisTest {
+
+  @Test
+  public void testTrue() {
+    Assertions.assertTrue(true);
+  }
+}
