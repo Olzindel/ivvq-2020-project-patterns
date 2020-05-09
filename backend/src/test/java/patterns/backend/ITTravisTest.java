279f25fb472d7bc5d4f7107f9dbad33a57459489
alexellero <alexandre.ellero@gmail.com>
2020-05-10 00:26:51 +0200

    chore : add jacoco IT checking #1

---
+++ backend/src/test/java/patterns/backend/ITTravisTest.java
@@ -0,0 +1,12 @@
+package patterns.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ITTravisTest {
+
+  @Test
+  public void testTrue() {
+    Assertions.assertTrue(true);
+  }
+}
