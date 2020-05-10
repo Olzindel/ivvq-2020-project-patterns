9fd3457b2287b8e6a8c70e8f5d53794f769ab8d1
pierre <5goupilis@gmail.com>
2020-05-10 16:55:10 +0200

    feat #30: add input name and firstName and improve verification of input.

--- frontend/src/components/user-option/UserAccount.vue
+++ frontend/src/components/user-option/UserAccount.vue
@@ -5,8 +5,17 @@
     </div>
     <div class="block" >
       <b-field grouped position="is-centered">
-        <b-field label="Nom et prenom" label-position='inside' expanded>
-          <b-input v-model="user.fullName"/>
+        <b-field label="Personal title" label-position='inside' >
+          <b-select v-model="user.gender">
+            <option value="F">Mme</option>
+            <option value="M">Mr</option>
+          </b-select>
+        </b-field>
+        <b-field label="prenom" label-position='inside' expanded>
+          <b-input v-model="user.firstName"/>
+        </b-field>
+        <b-field label="Nom" label-position='inside' expanded>
+          <b-input v-model="user.name"/>
         </b-field>
       </b-field>
         <b-field position="is-left" label="date d'anniversaire" label-position='inside' style="margin-right: 50%">
@@ -22,14 +31,14 @@
           <b-input v-model="user.street"/>
         </b-field>
         <b-field label="Code Postal" label-position='inside' expanded>
-          <b-input v-model="user.postalCode"/>
+          <b-input minlength="5" maxlength="5" pattern="[0-9]*" v-model="user.postalCode"/>
         </b-field>
       </b-field>
         <b-field position="is-left" label="Ville" label-position='inside' style="margin-right: 50%">
           <b-input v-model="user.town"/>
         </b-field>
       <b-field label="email" label-position='inside' expanded>
-        <b-input type="email" v-model="user.email"></b-input>
+        <b-input placeholder="Email" type="email" v-model="user.email"></b-input>
       </b-field>
       <b-field label="password" label-position='inside' expanded>
         <b-input type="password"/>
@@ -48,7 +57,9 @@ export default {
   data () {
     return {
       user: {
-        fullName: 'Jean Martin',
+        firstName: 'Jean',
+        name: 'MARTIN',
+        gender: 'M',
         email: 'waifu@market-desu.com',
         dateOfBirth: new Date(),
         street: '118 route de Narbonne',
