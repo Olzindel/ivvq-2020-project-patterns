a01d1d4de596965e0ca6f51037f062cce41b60db
pierre <5goupilis@gmail.com>
2020-05-10 15:18:42 +0200

    feat #30: add userAccount page with a dummy user

---
+++ frontend/src/components/user-option/UserAccount.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>Mes informations</h1>
+    </div>
+    <div class="block" >
+      <b-field grouped position="is-centered">
+        <b-field label="Nom et prenom" label-position='inside' expanded>
+          <b-input v-model="user.fullName"/>
+        </b-field>
+      </b-field>
+        <b-field position="is-left" label="date d'anniversaire" label-position='inside' style="margin-right: 50%">
+        <b-datepicker
+          ref="datepicker"
+          expanded
+          placeholder="Select a date"
+        v-model="user.dateOfBirth">
+        </b-datepicker>
+        </b-field>
+      <b-field grouped  position="is-centered">
+        <b-field label="Rue" label-position='inside' expanded>
+          <b-input v-model="user.street"/>
+        </b-field>
+        <b-field label="Code Postal" label-position='inside' expanded>
+          <b-input v-model="user.postalCode"/>
+        </b-field>
+      </b-field>
+        <b-field position="is-left" label="Ville" label-position='inside' style="margin-right: 50%">
+          <b-input v-model="user.town"/>
+        </b-field>
+      <b-field label="email" label-position='inside' expanded>
+        <b-input type="email" v-model="user.email"></b-input>
+      </b-field>
+      <b-field label="password" label-position='inside' expanded>
+        <b-input type="password"/>
+      </b-field>
+      <b-field label="password" label-position='inside' expanded>
+        <b-input type="password" />
+      </b-field>
+    </div>
+    <b-button type="is-primary" class="buttonValidation" size="is-Large" @click="updateInfo">Je change mes informations</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserAccount',
+  data () {
+    return {
+      user: {
+        fullName: 'Jean Martin',
+        email: 'waifu@market-desu.com',
+        dateOfBirth: new Date(),
+        street: '118 route de Narbonne',
+        postalCode: '31500',
+        town: 'Toulouse'
+      }
+    }
+  },
+  methods: {
+    updateInfo () {
+      console.log(this.user)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  size: 25px;
+}
+.block{
+  padding: 24px;
+  margin: 0
+}
+  .buttonValidation{
+    margin-bottom: 30px;
+  }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,10 +13,12 @@ const httpLinkProd = new HttpLink({
   uri: 'http://' + window.location.host + '/graphql'
 })
 
-/*const httpLinkDev = new HttpLink({
+/*
+const httpLinkDev = new HttpLink({
   // URL to graphql server, you should use an absolute URL here
   uri: 'http://localhost:8080/graphql'
-})*/
+})
+*/
 
 // create the apollo client
 const apolloClient = new ApolloClient({
@@ -31,7 +33,6 @@ const apolloProvider = new VueApollo({
 // install the vue plugin
 Vue.use(VueApollo)
 Vue.config.productionTip = false
-console.log(test)
 
 /* eslint-disable no-new */
 new Vue({
