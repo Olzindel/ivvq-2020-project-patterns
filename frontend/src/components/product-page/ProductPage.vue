77a6c8133d8a035fd907cdc538314b48f3380c50
Olzindel <timo.thibault@gmail.com>
2020-05-16 01:43:13 +0200

    wip: do not merge

--- frontend/src/components/product-page/ProductPage.vue
+++ frontend/src/components/product-page/ProductPage.vue
@@ -1,38 +1,62 @@
 <template>
-  <div>I am the page for id {{productId}}
-    <div class="columns">
-      <ApolloQuery
-        :query="require('/graphql/HelloWorld.gql')"
-        :variables="{ name }">
-        <template slot-scope="{ result: { loading, error, data } }">
-          <!-- Loading -->
-          <div v-if="loading" class="loading apollo">Loading...</div>
+  <div>
+    <div v-if="$apollo.queries.productInfos.error">
+      Erreur du chargement du produit...
+    </div>
 
-          <!-- Error -->
-          <div v-else-if="error" class="error apollo">An error occured</div>
+    <div v-else-if="$apollo.queries.productInfos.loading">
+      <b-icon class="icon loading-icon"></b-icon>
+    </div>
 
-          <!-- Result -->
-          <div v-else-if="data" class="result apollo">{{ data.hello }}</div>
-
-          <!-- No result -->
-          <div v-else class="no-result apollo">No result :(</div>
-        </template>
-      </ApolloQuery>
-      <div class="column is-two-fifths">
-        <vueper-slides :bullets="carousel.options.useBulletPoints" :arrows="carousel.options.useArrow">
+    <div class="columns main-layout is-mobile" v-else>
+      <div class="column is-5 productImage">
+        <vueper-slides :arrows="carousel.options.useArrow"
+                       :bullets="carousel.options.useBulletPoints"
+                       :fixed-height="true" :touchable="false"
+                       fade lazy
+                       lazy-load-on-drag>
           <template v-slot:arrow-left>
             <i class="icon icon-arrow-left"/>
           </template>
-
           <template v-slot:arrow-right>
             <i class="icon icon-arrow-right"/>
           </template>
+          <vueper-slide :image="image.imageLink"
+                        :key="i"
+                        v-for="(image, i) in productInfos.imageLinks"
+                        v-if="productInfos.imageLinks.length">
+            <template v-slot:loader>
+              <b-icon class="icon icon-loader spinning"/>
+            </template>
+          </vueper-slide>
 
-          <vueper-slide v-for="(slide, i) in carousel.slides" :key="i" :title="slide.title" :content="slide.content"/>
+          <vueper-slide image="../../assets/product-page/no-image-icon.png" v-else>
+            <template v-slot:loader>
+              <i class="icon icon-loader spinning"></i>
+              <b-icon class="icon loading-icon">
+                <span>Loading...</span>
+              </b-icon>
+            </template>
+          </vueper-slide>
         </vueper-slides>
       </div>
-      <div class="column">
-        Description will come there
+
+      <div class="column productInfos is-vcentered is-centered">
+        <div class="columns is-multiline">
+          <div class="column is-12 section box">
+            <h1 class="title">{{productInfos.name}}</h1>
+            <h2 class="subtitle">{{productInfos.price}} €</h2>
+            <article class="description" v-if="productInfos.description">
+              {{productInfos.description}}
+            </article>
+            <div v-else>
+              Aucune description pour ce produit.
+            </div>
+          </div>
+          <div class="column add-to-basket">
+            <b-button @click="clickMe">Click Me</b-button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -45,30 +69,51 @@ import Router from 'vue-router'
 import {VueperSlide, VueperSlides} from 'vueperslides'
 import 'vueperslides/dist/vueperslides.css'
 
+import gql from 'graphql-tag'
+
 Vue.use(Router)
 
 export default {
   name: 'ProductPage',
-  props: ['productId'],
   components: {VueperSlides, VueperSlide},
+  props: ['productId'],
+  apollo: {
+    productInfos () {
+      return {
+        query: gql`query ProductInfos($productId: ID!) {
+          productInfos: product(productId: $productId){
+            name
+             imageLinks{
+              imageLink
+             }
+            price
+            status
+            description
+          }
+        }`,
+        variables () {
+          return {
+            productId: this.productId
+          }
+        }
+      }
+    }
+  },
 
   data () {
     return {
+      productInfos: {
+        name: '',
+        imageLinks: [],
+        price: '',
+        status: '',
+        description: ''
+      },
       carousel: {
         options: {
           useArrow: true,
           useBulletPoints: true
-        },
-        slides: [
-          {
-            title: 'Slide <b style="font-size: 1.3em;color: yellow">#1</b>',
-            content: 'Slide title can be HTML.<br>And so does the slide content, <span style="font-size: 1.2em;color: yellow">why not?</span>'
-          },
-          {
-            title: 'Slide <b style="font-size: 1.3em;color: yellow">#2</b>',
-            content: 'Slide title can be HTML.<br>And so does the slide content, <span style="font-size: 1.2em;color: yellow">why not?</span>'
-          }
-        ]
+        }
       }
     }
   }
@@ -76,21 +121,47 @@ export default {
 </script>
 
 <style scoped>
-  .icon-arrow-right:before {
-    background-image: url('../../assets/product-page/pointingRight.svg');
-    background-size: 32px 32px;
-    display: block;
+  .icon-arrow-left:before {
+    content: url('../../assets/product-page/pointingLeft.svg');
+    display: inline-flex;
     width: 32px;
     height: 32px;
-    content: "";
+    background: white;
+    border-radius: 32px;
   }
 
-  .icon-arrow-left:before {
-    background-image: url("../../assets/product-page/pointingLeft.svg");
-    background-size: 32px 32px;
-    display: block;
+  .icon-arrow-right:before {
+    content: url('../../assets/product-page/pointingRight.svg');
+    display: inline-flex;
     width: 32px;
     height: 32px;
-    content: "";
+    background: white;
+    border-radius: 32px;
+  }
+
+  .productInfos {
+    text-align: left;
+  }
+
+  .productImage {
+    max-width: 100%;
+    height: auto;
+  }
+
+  .vueperslide--loading .vueperslide__content-wrapper {
+    display: none !important;
+  }
+
+  .vueperslides--fixed-height {
+    height: 500px;
+  }
+
+  .main-layout {
+    padding: 15px 15px 15px 15px;
+  }
+
+  .add-to-basket {
+    display: flex;
+    align-items: center;
   }
 </style>
