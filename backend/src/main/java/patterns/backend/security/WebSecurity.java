3f2979768edf2a2e27ee9b485612fdbf1c81ecc1
Olzindel <timo.thibault@gmail.com>
2020-05-29 22:58:48 +0200

    fix: make ci/cd works

--- backend/src/main/java/patterns/backend/security/WebSecurity.java
+++ backend/src/main/java/patterns/backend/security/WebSecurity.java
@@ -15,59 +15,59 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-    private UserDetailsServiceImpl userDetailsService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+  private UserDetailsServiceImpl userDetailsService;
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public WebSecurity(
-            UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+  public WebSecurity(
+      UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.userDetailsService = userDetailsService;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/login**")
-                .permitAll()
-                .antMatchers("/graphql**")
-                .permitAll()
-                .antMatchers("/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService))
-                // this disables session creation on Spring Security
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/login**")
+        .permitAll()
+        .antMatchers("/graphql**")
+        .permitAll()
+        .antMatchers("/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+        .addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService))
+        // this disables session creation on Spring Security
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                // .and().formLogin()
-                // .loginPage("/login").permitAll()
+        // .and().formLogin()
+        // .loginPage("/login").permitAll()
 
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true);
-    }
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/")
+        .invalidateHttpSession(true)
+        .clearAuthentication(true);
+  }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+  }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-        config.addExposedHeader("Authorization");
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+    config.addExposedHeader("Authorization");
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
 }
